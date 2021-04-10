1ce66a6421e5e2aed6feb754cf2332316463972e
hprabesh <53958606+hprabesh@users.noreply.github.com>
2021-04-10 17:43:27 -0500

    working on login- registration

--- app/src/main/java/com/example/performance_tracker/MainActivity.java
+++ app/src/main/java/com/example/performance_tracker/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.performance_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+
+        Button login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
