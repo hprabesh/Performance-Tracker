c69b26cc6a00d90cc46086e559c8da0701ac670a
hprabesh <53958606+hprabesh@users.noreply.github.com>
2021-04-10 20:04:17 -0500

    Login and Registration Layout

--- app/src/main/java/com/example/performance_tracker/MainActivity.java
+++ app/src/main/java/com/example/performance_tracker/MainActivity.java
@@ -7,21 +7,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText username = findViewById(R.id.username);
-        EditText password = findViewById(R.id.password);
+//        private FirebaseAuth mAuth;
 
-        Button login = findViewById(R.id.login);
+        EditText usernameField = findViewById(R.id.username);
+        EditText passwordField = findViewById(R.id.password);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        Button loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String usernameString = ((EditText) usernameField).getText().toString();
+                String passwordString = ((EditText) passwordField).getText().toString();
+                if (usernameString.equals("prabesh") && passwordString.equals("prabesh")){
+                        Snackbar.make(findViewById(R.id.login_page),
+                            "Error Username", Snackbar.LENGTH_SHORT).show();
+                }
             }
         });
     }
