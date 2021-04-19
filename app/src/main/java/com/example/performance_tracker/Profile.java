06314fb91132dd6c55d92b3ec7b33ee0f7e15231
robert <rob.w.gleason@gmail.com>
2021-04-19 17:01:39 -0500

    add Friends and Project

--- app/src/main/java/com/example/performance_tracker/Profile.java
+++ app/src/main/java/com/example/performance_tracker/Profile.java
@@ -11,10 +11,30 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
     private Button logOut;
+    private Button addFriendButton;
+    private Button addProject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        addProject =(Button) findViewById(R.id.project);
+        addProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, AddProject.class));
+            }
+        });
+
+
+        addFriendButton =(Button) findViewById(R.id.friends);
+        addFriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, addFriends.class));
+            }
+        });
+
 
         logOut =(Button) findViewById(R.id.log_out);
         logOut.setOnClickListener(new View.OnClickListener() {
@@ -27,4 +47,6 @@ public class Profile extends AppCompatActivity {
             }
         });
     }
+
+
 }
